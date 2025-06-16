80eb4c5d6ceac45789c93c499b8633fc3afc2544
Yonel Ceruto <yonel.ceruto@cocolabs.com>
2025-06-16 19:18:29 +0200

    Update index.js

--- src/Resources/assets/admin/js/index.js
+++ src/Resources/assets/admin/js/index.js
@@ -4,7 +4,7 @@
  * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
  */
 
-import './emailTemplatePreview';
+//import './emailTemplatePreview';
 
 require('codemirror/mode/htmlmixed/htmlmixed');
 require('codemirror/mode/css/css');
@@ -13,13 +13,13 @@ const CodeMirror = require('codemirror/lib/codemirror');
 document.addEventListener('DOMContentLoaded', () => {
     const codemirrorTextarea = document.querySelectorAll('.codemirror-editor');
     const codeMirrorInstances = [];
-    const accordions = $('[name="bitbag_sylius_mail_template_plugin_template_email"] .ui.accordion');
+    //const accordions = $('[name="bitbag_sylius_mail_template_plugin_template_email"] .ui.accordion');
 
-    accordions.on('click', () => {
-        codeMirrorInstances.forEach((codeMirrorInstance) => {
-            codeMirrorInstance.refresh();
-        });
-    });
+    //accordions.on('click', () => {
+    //    codeMirrorInstances.forEach((codeMirrorInstance) => {
+    //        codeMirrorInstance.refresh();
+    //    });
+    //});
 
     codemirrorTextarea.forEach((textarea) => {
         const codemirror = CodeMirror.fromTextArea(textarea, {
